52da19d3b05b5f87369390843245581ab55ea076
jesusrlv dell <jesusrlvrojo@gmail.com>
2024-01-25 00:50:45 -0600

    ya saca los datos de conteo, solo falta acomodarlos

--- js/querys.js
+++ js/querys.js
@@ -32,8 +32,6 @@ function calendarioQuery() {
     }
     else{
 
-      
-
     document.getElementById("calendarioGrid").innerHTML = "";
     var diasMes = new Date(annio, mes, 0).getDate();
     var diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
@@ -47,9 +45,12 @@ function calendarioQuery() {
           type: "POST",
           url: 'query/queryActividadesCalendario.php',
           dataType:'html',
-          
+          data:{
+            mes:mes,
+            annio:annio
+          },
           success: function (data) {
-            var jsonData = JSON.parse(data)
+            var jsonData = JSON.parse(data);
             console.log('Respuesta JSON:', jsonData);
     
             // Asegúrate de que jsonData sea un array antes de intentar acceder a su longitud
@@ -92,7 +93,7 @@ function calendarioQuery() {
             
             // document.getElementById("datosNm").innerHTML += '<span class="badge me-1 rounded-pill text-bg-primary">9</span>';
                 
-              document.getElementById("calendarioGrid").innerHTML += '<div class="col-lg-2 col-md-4 col-sm-6"><div class="card mb-3" style="max-width: 540px;"><div class="row g-0"><div class="col-md-4 my-auto"><h1 class="text-center">'+dia+'</h1></div><div class="col-md-8 bg-primary"><div class="card-body"><h5 class="card-title text-light">'+diasSemana[indice]+'</h5><p class="card-text text-light">Actividades</p><p class="card-text text-light"><small class="text-body-light" id="datosNm'+dia+'"><span class="badge me-1 rounded-pill text-bg-primary">9</span><span class="badge me-1 rounded-pill text-bg-secondary">9</span><span class="badge me-1 rounded-pill text-bg-success">9</span><span class="badge me-1 rounded-pill text-bg-danger">9</span><span class="badge me-1 rounded-pill text-bg-warning">9</span><span class="badge me-1 rounded-pill text-bg-info">9</span><span class="badge me-1 rounded-pill text-bg-light">9</span><span class="badge me-1 rounded-pill text-bg-dark">9</span></small></p></div></div></div></div></div><!--col-->';
+              document.getElementById("calendarioGrid").innerHTML += '<div class="col-lg-2 col-md-4 col-sm-6"><div class="card mb-3" style="max-width: 540px;"><div class="row g-0"><div class="col-md-4 my-auto"><h1 class="text-center">'+dia+'</h1></div><div class="col-md-8 bg-primary"><div class="card-body"><h5 class="card-title text-light">'+diasSemana[indice]+'</h5><p class="card-text text-light">Actividades</p><p class="card-text text-light"><small class="text-body-light" id="datosNm'+dia+'"></small></p></div></div></div></div></div><!--col-->';
           
           }
       //   }
--- query/queryActividadesCalendario.php
+++ query/queryActividadesCalendario.php
@@ -2,15 +2,21 @@
     session_start();
     include('../prcd/qc.php');
     // $usr = $_SESSION['usr'];
+    $mes = $_POST['mes'];
+    $annio = $_POST['annio'];
 
 $sql = "SELECT * FROM users WHERE perfil = 2";
 $resultado = $conn->query($sql);
 while ($row = $resultado->fetch_assoc()){
     $usr = $row['id'];
     $color = $row['color']; 
-    $sqlActividades = "SELECT * FROM actividades WHERE responsable = '$usr'";
+    $sqlActividades = "SELECT * FROM actividades WHERE responsable = '$usr' AND month(fecha) = $mes AND year(fecha) = $annio";
     $resultadoAct = $conn->query($sqlActividades);
     $filaAct = $resultadoAct->num_rows;
+    
+    /* $fecha = new DateTime($row['fecha']);
+    $mes = $fecha->format('m'); // Obtiene el día del mes (01-31)
+    $annio = $fecha->format('Y'); // Obtiene el año de cuatro dígitos */
 
     // echo $usr.'<br>';
     // echo $color.'<br>';
@@ -19,7 +25,9 @@ while ($row = $resultado->fetch_assoc()){
     $usuarios[] = array(
         'usr' => $usr,
         'actividad' => $filaAct,
-        'color' => $color
+        'color' => $color,
+        'mes' => $mes,
+        'annio' => $annio,
     );
 }
 echo json_encode($usuarios);
